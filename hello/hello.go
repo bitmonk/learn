04467d129b7603d01454c407bd8aebbe319732a1
Justin Alan Ryan <justizin@justiz.in>
2021-11-16 06:07:00 +0000

    multiple welcomes with slices and prng

--- hello/hello.go
+++ hello/hello.go
@@ -14,13 +14,16 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// If no error was returned, print the returned message
 	// to the console
-	fmt.Println(message)
+	fmt.Println(messages)
 }
