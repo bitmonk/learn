1fe17557b83e9591812dd9b3f5df4bdb49d632d9
Justin Alan Ryan <justizin@justiz.in>
2022-04-29 21:39:22 +0000

    use plus-equals assignment.

--- src/golang/with-tests/iteration/repeat.go
+++ src/golang/with-tests/iteration/repeat.go
@@ -1,8 +1,9 @@
 package iteration
 
+// Repeat takes a character and repeats it 5 times.
 func Repeat(character string) (repeated string) {
 	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+		repeated += character
 	}
 	return
 }
