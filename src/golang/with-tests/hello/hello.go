d919ef2c0bf520245b43c36d30dfb328efb390a2
Justin Alan Ryan <justizin@justiz.in>
2022-04-29 20:20:13 +0000

    refactor switch statement into separate function.

--- src/golang/with-tests/hello/hello.go
+++ src/golang/with-tests/hello/hello.go
@@ -10,17 +10,20 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Spanish":
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return prefix
 }
 
 func main() {
