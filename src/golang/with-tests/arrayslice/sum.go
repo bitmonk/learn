4784ad35a5aeac064d5969c8083ee1a1d0d9a6fa
Justin Alan Ryan <justizin@justiz.in>
2022-04-29 22:45:15 +0000

    implement SumAllTails and test it.

--- src/golang/with-tests/arrayslice/sum.go
+++ src/golang/with-tests/arrayslice/sum.go
@@ -13,3 +13,11 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 	}
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return
+}
