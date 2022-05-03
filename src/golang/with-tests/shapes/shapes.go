257aa39e8b325460743171fe204404fbff61b18a
Justin Alan Ryan <justizin@justiz.in>
2022-05-03 02:48:56 +0000

    implement area.

--- src/golang/with-tests/shapes/shapes.go
+++ src/golang/with-tests/shapes/shapes.go
@@ -4,3 +4,8 @@ func Perimeter(width, height float64) (perim float64) {
 	perim = 2 * (width + height)
 	return
 }
+
+func Area(width, height float64) (area float64) {
+	area = width * height
+	return
+}
