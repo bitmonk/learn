972ecbe33617a36e4c351ea358a37c824bafa97f
Justin Alan Ryan <justizin@justiz.in>
2022-05-03 04:12:30 +0000

    refactor to use methods.

--- src/golang/with-tests/shapes/shapes.go
+++ src/golang/with-tests/shapes/shapes.go
@@ -5,12 +5,21 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rect Rectangle) (perim float64) {
 	perim = 2 * (rect.Width + rect.Height)
 	return
 }
 
-func Area(rect Rectangle) (area float64) {
-	area = rect.Width * rect.Height
+func (r Rectangle) Area() (area float64) {
+	area = r.Width * r.Height
+	return
+}
+
+func (c Circle) Area() (area float64) {
+	area = 0
 	return
 }
