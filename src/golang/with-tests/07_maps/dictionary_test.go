336f8f98b3a698951a9940d3398fcc43b2e762a5
Justin Alan Ryan <justizin@gmail.com>
2022-12-30 19:45:12 -0800

    define reusable error

--- src/golang/with-tests/07_maps/dictionary_test.go
+++ src/golang/with-tests/07_maps/dictionary_test.go
@@ -14,13 +14,13 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		want := ErrNotFound
 
 		if err == nil {
 			t.Fatal("expected to get an error")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, err, want)
 	})
 }
 
@@ -28,5 +28,12 @@ func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
